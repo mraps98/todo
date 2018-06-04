00563be983a94a53e6efccb78834bdcd3278f62e
Preet <amritpreet98@gmail.com>
2018-06-04 23:37:38 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    "use strict";
+
+    var App = {
+        todo : ["groceries","homework"],
+        done : ["firstday","gitcommit"],
+    };
+
+    App.start = function(){
+        App.update();
+    };
+
+    App.update = function(){
+        $("#todolist").empty();
+        $("#donelist").empty();
+        for(var i = 0; i < App.todo.length; i++){
+            $("#todolist").append("<li><button class='donebutton' data-text='" + App.todo[i] + "'>Done</button>" + App.todo[i] + "</li>");
+        }
+        for(var i = 0; i < App.done.length; i++){
+            $("#donelist").append("<li><button class='removebutton' data-text='" + App.done[i] + "'>Remove</button>" + App.done[i] + "</li>");
+        }
+    };
+
+    $(document).on('click','#addbutton',function(){
+        console.log("add");
+        let temp = $("#todoinput").val();
+        App.todo.push(temp);
+        App.update();
+    });
+
+    $(document).on('click',".donebutton",function(){
+        console.log("done");
+        let temp = $(this).attr("data-text");
+        let index = App.todo.indexOf(temp);
+        App.todo.splice(index, 1);
+        App.done.push(temp);
+        App.update();
+    });
+
+    $(document).on('click',".removebutton",function(){
+        console.log("remove");
+        let temp = $(this).attr("data-text");
+        let index = App.done.indexOf(temp);
+        App.done.splice(index,1);
+        App.update();
+    });
+
+
+
+    App.start();
+});
